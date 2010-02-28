ab4ee270ad39c8b8dc9e38ef681c0b62dc9ebe45
Philip Hofstetter <phofstetter@sensational.ch>
2010-02-28 21:41:47 +0100

    retrieve list of exceptions

--- lib/controllers/exceptions.php
+++ lib/controllers/exceptions.php
@@ -5,7 +5,31 @@ RequestMapper::registerRequest('#^/exceptions#', '/exceptions', 'ExceptionContro
 class ExceptionController extends BaseController{
 
     function handle($url){
-        return array('gnegg' => 'blepp');
+        list($name, $exid, $data) = explode('/', $url);
+        if ($data){
+            throw new BadRequest('Syntax: /exceptions[/id]');
+        }
+        if ($extid){
+            return $this->getException($exid);
+        }else{
+            return $this->getExceptionList();
+        }
+    }
+
+    private function getException($exid){
+
+    }
+
+    private function getExceptionList(){
+        $limit = isset($_GET['pagesize']) ? $_GET['pagesize'] : 10;
+
+        $db = Database::getDatabase();
+        $q = sprintf(
+            "select '/exceptions/'||id as href, id, uri, type, ts from exception_logging order by ts desc limit %d",
+            $limit
+        );
+
+        return $db->queryArray($q);
     }
 
 }
