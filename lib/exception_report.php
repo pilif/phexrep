0eeb13ad06644779463fd4af73c8286f7f3d5928
Philip Hofstetter <phofstetter@sensational.ch>
2010-03-01 16:55:30 +0100

    include message in overview

--- lib/exception_report.php
+++ lib/exception_report.php
@@ -6,7 +6,7 @@ class ExceptionReport implements ArrayAccess {
     static function getReports($limit = 10){
         $db = Database::getDatabase();
         $q = sprintf(
-            "select id, uri, type, ts from exception_logging order by ts desc limit %d",
+            "select id, uri, type, ts, message from exception_logging order by ts desc limit %d",
             $limit
         );
         return $db->queryArray($q);
